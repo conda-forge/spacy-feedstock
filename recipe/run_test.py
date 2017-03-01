8545b5edb1774c7ba3b1216a2f9bb729c5a80da9
Rolando Espinoza <rndmax84@gmail.com>
2017-03-01 19:27:13 -0300

    Update recipe to version 1.2.0

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,6 +1,7 @@
 import os
+import sys
 import pytest
 import spacy
 
 PACKAGE_DIR = os.path.abspath(os.path.dirname((spacy.__file__)))
-pytest.main([PACKAGE_DIR])
+sys.exit(pytest.main([PACKAGE_DIR]))
