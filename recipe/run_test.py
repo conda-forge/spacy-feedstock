e46428212828c84e97e3180dc137dbe4ee6f22c4
Adriane Boyd <adrianeboyd@gmail.com>
2020-05-13 09:08:29 +0200

    Add multiprocess.freeze_support() for spawn

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -2,6 +2,9 @@ import os
 import sys
 import pytest
 import spacy
+import multiprocessing
+
+multiprocessing.freeze_support()
 
 PACKAGE_DIR = os.path.abspath(os.path.dirname((spacy.__file__)))
 sys.exit(pytest.main([PACKAGE_DIR]))
