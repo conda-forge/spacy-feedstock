8b31a8505efdaad7e9b7579d69f6911b56208cbe
Adriane Boyd <adrianeboyd@gmail.com>
2020-05-13 09:46:21 +0200

    Update test script

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -4,7 +4,8 @@ import pytest
 import spacy
 import multiprocessing
 
-multiprocessing.freeze_support()
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
 
-PACKAGE_DIR = os.path.abspath(os.path.dirname((spacy.__file__)))
-sys.exit(pytest.main([PACKAGE_DIR]))
+    PACKAGE_DIR = os.path.abspath(os.path.dirname((spacy.__file__)))
+    sys.exit(pytest.main([PACKAGE_DIR]))
